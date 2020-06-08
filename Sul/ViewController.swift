77d6c3e11a00b6263c4d3611227865b3301a7d07
raon29 <raon0229@gmail.com>
2020-06-08 18:49:04 +0900

    ios first

---
+++ Sul/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sul
+//
+//  Created by misong lee on 08/06/2020.
+//  Copyright © 2020 misong lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
