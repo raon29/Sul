b05684f4b802a27bf146ebf1e8ff784ca21bc34c
raon29 <raon0229@gmail.com>
2020-06-28 22:57:57 +0900

    첫 화면

--- Sul_IOS/Sul/ViewController.swift
+++ Sul_IOS/Sul/ViewController.swift
@@ -8,13 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+
+    @IBOutlet weak var resultSul: UILabel!
+    @IBOutlet weak var inputMoney: UITextField!
+
+    @IBOutlet weak var changeBtn: UIButton!
+    @IBOutlet weak var settingBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        //input number pad로 설정
+        inputMoney.keyboardType = .numberPad
+        
+        
+        
+        
     }
-
-
+    
+    // setting Button 눌렀을 때
+    @IBAction func settingClick(_ sender: Any) {
+        print("settnig click!")
+    }
+    // change Button 눌렀을 때
+    @IBAction func changeClick(_ sender: Any) {
+        print("change click!")
+    }
+    
+    // 터치시 keyboard 닫기 설정
+     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+          self.view.endEditing(true)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print( "편집 종료" )
+        print(textField)
+    }
+    
 }
 
